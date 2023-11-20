b620141292e358e2499a9096eaa0d8b47e32ee2c
JB <drv98@naver.com>
2023-11-20 10:15:52 +0900

    카드만들기 & 픽사베이키설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import './App.css';
-
 function App() {
-  return <h1 className="text-3xl font-semibold underline">Hello world!</h1>;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">Photo by 홍길동</div>
+        <ul>
+          <li>
+            <strong>뷰: </strong> 4000
+          </li>
+          <li>
+            <strong>다운로드: </strong> 300
+          </li>
+          <li>
+            <strong>추천수: </strong> 400
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4 text-sm font-semibold text-gray-700">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 mr-2">#tag1</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 mr-2">#tag2</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 mr-2">#tag3</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
